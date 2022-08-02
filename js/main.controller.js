9175d1b9f0f3aa13c43b2dddb65882f352c93356
Yoav <yoav.sher11@gmail.com>
2022-08-02 18:17:47 +0300

    adding heb option and some bootstrap

--- js/main.controller.js
+++ js/main.controller.js
@@ -5,6 +5,7 @@ console.log('hi');
 function onInit() {
     renderBooks()
     renderFilterByQueryStringParams()
+    doTrans()
 }
 
 
@@ -16,17 +17,15 @@ function renderBooks() {
         <td>${book.id}</td>
         <td>${book.title}</td>
         <td>${book.price}$</td>
-        <td><button class="read-book" onclick="onReadBook('${book.id}')">Read</button></td>
-        <td><button class="update-book" onclick="onUpdateBook('${book.id}')">Update</button></td>
-        <td><button class="remove-book" onclick="onRemoveBook('${book.id}')">Remove</button></td>
+        <td><button data-trans="read" class="read-book" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="onReadBook('${book.id}')">Read</button></td>
+        <td><button data-trans="update" class="update-book" onclick="onUpdateBook('${book.id}')">Update</button></td>
+        <td><button data-trans="remove" class="remove-book" onclick="onRemoveBook('${book.id}')">Remove</button></td>
      </tr>`
     ).join('')
-    strHtml = `<tr> <th>ID</th><th onclick="onSetSortBy(this)">TITLE</th><th onclick="onSetSortBy(this)">PRICE</th>
-    <th colspan="3">Actions</th></tr>` + strHtml
-    document.querySelector('.books-table').innerHTML = strHtml
+    document.querySelector('.books-table tbody').innerHTML = strHtml
     // console.log('im render');
     if (gPageIdx === 0) {
-        document.querySelector('.prev-page').classList.add('disabled-btn')
+        document.querySelector('.prev-page').setAttribute('disabled','')
     }
 }
 
@@ -55,17 +54,16 @@ function onUpdateBook(bookId) {
 }
 
 function onReadBook(bookId) {
+    console.log('bookId:', bookId)
     const book = getBookById(bookId)
-    const elModal = document.querySelector('.modal')
-    // const elRate = document.querySelector('.rate span')
-    // console.log('elModal:', elModal)
-    console.log('book:', book.title)
-    elModal.querySelector('h3 span').innerText = book.title
-    elModal.querySelector('h4 span').innerText = `${book.price}$`
+    const elModal = document.querySelector('.my-modal')
+    console.log('elModal:', elModal)
+    elModal.querySelector('h3 .modal-title').innerText = book.title
+    elModal.querySelector('h4 .modal-price').innerText = `${book.price}$`
     elModal.querySelector('p').innerText = makeLorem()
-    elModal.querySelector('.rate span').innerText = ` ${book.rate} `
-    gRate = +elModal.querySelector('.rate span').innerText
-    elModal.classList.add('open')
+   elModal.querySelector('.rate .modal-rate').innerText = ` ${book.rate} `
+    gRate = +elModal.querySelector('.rate .modal-rate').innerText
+    // elModal.classList.add('open')
     console.log('book.id:', book.id)
     console.log('book.rate:', book.rate)
 }
@@ -73,18 +71,18 @@ function onReadBook(bookId) {
 function onCloseModal() {
     const book = getBookById(gId)
     getBookRate(book.id)
-    const elModal = document.querySelector('.modal')
-    elModal.classList.remove('open')
+    // const elModal = document.querySelector('.modal')
+    // elModal.classList.remove('open')
 }
 
 function onRateDown() {
     const book = getBookById(gId)
-    const elRate = document.querySelector('.rate span')
+    const elRate = document.querySelector('.modal-footer .rate .modal-rate')
     if (gRate > 0) {
         console.log('gRate', gRate)
         gRate -= 1
         console.log('book.rate:', book.rate)
-        elRate.innerText = ` ${gRate} `
+        elRate.innerText = `  ${gRate}  `
         // console.log('rate:', rate)
     }
     return gRate
@@ -92,12 +90,12 @@ function onRateDown() {
 
 function onRateUp() {
     const book = getBookById(gId)
-    const elRate = document.querySelector('.rate span')
-
+    const elRate = document.querySelector('.modal-footer .rate .modal-rate')
+console.log('elRate.innerText:', elRate.innerText)
     console.log('book.rate:', book.rate)
     if (gRate < 10) {
         gRate += 1
-        elRate.innerText = ` ${gRate} `
+        elRate.innerText = `  ${gRate}  `
     }
     console.log('gRate', gRate)
     return gRate
@@ -158,10 +156,10 @@ function onNextPage() {
     nextPage()
     if (Math.floor(gBooks.length / PAGE_SIZE) <= gPageIdx) {
         console.log('gPageIdx:', gPageIdx)
-        document.querySelector('.next-page').classList.add('disabled-btn')
+        document.querySelector('.next-page').setAttribute('disabled','')
     } else {
-        document.querySelector('.next-page').classList.remove('disabled-btn')
-        document.querySelector('.prev-page').classList.remove('disabled-btn')
+        document.querySelector('.next-page').removeAttribute('disabled')
+        document.querySelector('.prev-page').removeAttribute('disabled')
     }
     renderBooks()
 }
@@ -169,9 +167,16 @@ function onNextPage() {
 function onPrevPage() {
     prevPage()
     if (gPageIdx > 0) {
-        document.querySelector('.prev-page').classList.remove('disabled-btn')
-        document.querySelector('.next-page').classList.remove('disabled-btn')
+        document.querySelector('.prev-page').removeAttribute('disabled')
+        document.querySelector('.next-page').removeAttribute('disabled')
     }
     renderBooks()
 }
 
+function onSetLang(lang) {
+    setLang(lang)
+    if (lang === 'he') document.body.classList.add('rtl') 
+    else document.body.classList.remove('rtl')
+    doTrans()
+    renderBooks()
+}
---
+++ js/services/i18n.service.js
@@ -0,0 +1,95 @@
+'use strict'
+
+const gTrans = {
+    title: {
+        en: 'Book Shop',
+        he: 'חנות ספרים'
+    },
+    filter: {
+        en: 'Filter',
+        he: 'סנן'
+    },
+    'add-book': {
+        en: 'Add book',
+        he: 'הוסף ספר'
+    },
+    'book-search': {
+        en: 'Search',
+        he: 'חפש'
+    },
+    'max-price': {
+        en: 'Max price',
+        he: 'מחיר מקסימלי'
+    },
+    'min-rate': {
+        en: 'Min rate',
+        he: 'דירוג מינימלי'
+    },
+    'prev-pg': {
+        en: 'Previous page',
+        he: 'הדף הקודם'
+    },
+    'next-pg': {
+        en: 'Next page',
+        he: 'הדף הבא'
+    },
+    'book-title': {
+        en: 'Title:',
+        he: 'שם הספר:'
+    },
+    'book-price': {
+        en: 'Price:',
+        he: 'מחיר:'
+    },
+    'book-sum': {
+        en: 'Summery:',
+        he: 'תקציר:'
+    },
+    'book-rate': {
+        en: 'Please rate the book from 1-10',
+        he: 'דרג את הספר מ1 עד10'
+    },
+    actions: {
+        en: 'Actions',
+        he: 'פעולות'
+    },
+    read: {
+        en: 'Read',
+        he: 'פרטים'
+    },
+    update: {
+        en: 'Update',
+        he: 'עדכן'
+    },
+    remove: {
+        en: 'Remove',
+        he: 'הסר'
+    }
+}
+
+var gLang = 'en'
+
+function setLang(lang) {
+    gLang = lang
+}
+
+function getTrans(transKey) {
+    const key = gTrans[transKey]
+    // console.log('key:', key)
+    if (!key) return 'UNKNOWN'
+    var transVal = key[gLang]
+    transVal = !transVal ? key['en'] : key[gLang]
+    return transVal
+}
+
+function doTrans() {
+    const elDataTrans = document.querySelectorAll('[data-trans]')
+    // console.log('elDataTrans:', elDataTrans)
+    elDataTrans.forEach(dataTrans => {
+        const translateKey = dataTrans.dataset.trans
+        const translateVal = getTrans(translateKey)
+        // console.log('dataTrans:', dataTrans)
+        dataTrans.innerText = translateVal
+        if (dataTrans.placeholder !== undefined) dataTrans.placeholder = translateVal
+    })
+}
